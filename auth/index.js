6bd1747080e16e75be90609d11722cd794bd134c
Andres Mauricio Rodriguez Garzon <anmrodriguezga@unal.edu.co>
2025-06-03 10:41:52 -0500

    Clase 11: Autenticacion y autorizacion con JWT

--- auth/index.js
+++ auth/index.js
@@ -1,7 +1,43 @@
 const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const secret = config.jwt.secret;
 
 function sign(data) {
-    return jwt.sign(data, 'secreto');
+    return jwt.sign(data, secret);
+}
+
+function verifyToken(token) {
+    return jwt.verify(token, secret);
+}
+
+const check = {
+    own: function(req, owner) {
+        const decoded = decodeHeader(req);
+        console.log(decoded);
+    },
+}
+
+function getToken(authorization) {
+    if (!authorization) {
+        throw new Error('No viene token');
+    }
+    
+    if (authorization.indexOf('Bearer ') === -1) {
+        throw new Error('Formato invalido');
+    }
+    
+    let token = authorization.replace('Bearer ', '');
+
+    return token;
+}
+
+function decodeHeader(req) {
+    const authorization = req.headers.authorization || '';
+    const token = getToken(authorization);
+    const decoded = verifyToken(token);
+    req.user = decoded;
+    return decoded;
 }
 
 module.exports = {
