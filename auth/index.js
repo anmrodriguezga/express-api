d7d72aefbebf668bc7afa00dd15356222747957c
Andres Mauricio Rodriguez Garzon <anmrodriguezga@unal.edu.co>
2025-06-04 10:50:39 -0500

    Adicion de docker compose para Dev Containers

--- auth/index.js
+++ auth/index.js
@@ -25,11 +25,11 @@ const check = {
 
 function getToken(authorization) {
     if (!authorization) {
-        throw new Error('No viene token');
+        throw error('No viene token', 401);
     }
     
     if (authorization.indexOf('Bearer ') === -1) {
-        throw new Error('Formato invalido');
+        throw error('Formato invalido', 401);
     }
     
     let token = authorization.replace('Bearer ', '');
